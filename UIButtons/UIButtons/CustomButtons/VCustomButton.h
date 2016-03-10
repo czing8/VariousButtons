425792c21509b92a8bf7680e625268ab2cf20702
Vols <zheng7998@gmail.com>
2016-03-10 09:44:27 +0800

    no message

---
+++ UIButtons/UIButtons/CustomButtons/VCustomButton.h
@@ -0,0 +1,13 @@
+//
+//  VCustomButton.h
+//  UIButtons
+//
+//  Created by Vols on 16/3/9.
+//  Copyright © 2016年 Vols. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VCustomButton : UIButton
+
+@end
--- UIButtons/UIButtons/ViewController.m
+++ UIButtons/UIButtons/ViewController.m
@@ -10,6 +10,8 @@
 #import "LocationBtn.h"
 #import "VLineButton.h"
 
+#import "VCustomButton.h"
+
 
 @interface ViewController ()
 
@@ -17,6 +19,7 @@
 @property (nonatomic, strong) VLineButton * anotherButton;
 
 @property (nonatomic, strong) LocationBtn * locationBtn;
+@property (nonatomic, strong) VCustomButton * customBtn;
 
 
 @end
@@ -29,6 +32,7 @@
 
     
     [self.view addSubview:self.locationBtn];
+    [self.view addSubview:self.customBtn];
     
     [self.view addSubview:self.button];
     [self.view addSubview:self.anotherButton];
@@ -57,7 +61,7 @@
         _locationBtn.frame = CGRectMake(0, 60, 160, 40);
         _locationBtn.backgroundColor = [UIColor clearColor];
         [_locationBtn setImage:[UIImage imageNamed:@"location"] forState:UIControlStateNormal];
-        [_locationBtn setTitle:@"石家庄" forState:UIControlStateNormal];
+        [_locationBtn setTitle:@"石家庄石家庄石家庄石家庄石家庄" forState:UIControlStateNormal];
         
         [_locationBtn addTarget:self action:@selector(locationAction:) forControlEvents:UIControlEventTouchUpInside];
     }
@@ -65,12 +69,28 @@
 }
 
 
+- (VCustomButton *)customBtn{
+    if (!_customBtn) {
+        
+        _customBtn = [[VCustomButton alloc] init];
+        
+        _customBtn.frame = CGRectMake(0, 30, 160, 40);
+        _customBtn.backgroundColor = [UIColor clearColor];
+        [_customBtn setImage:[UIImage imageNamed:@"location"] forState:UIControlStateNormal];
+        [_customBtn setTitle:@"石家庄" forState:UIControlStateNormal];
+        
+        [_customBtn addTarget:self action:@selector(locationAction:) forControlEvents:UIControlEventTouchUpInside];
+    }
+    return _customBtn;
+}
+
+
 - (VLineButton *)button{
     if (!_button) {
         _button = [[VLineButton alloc] init];
-        _button.frame = CGRectMake(60, 100, 200, 60);
+        _button.frame = CGRectMake(60, 100, 100, 40);
         //    _button.titleLabel.font = [UIFont fontWithName:@"Avenir-Black" size:18.0];
-        [_button setTitle:@"Button One" forState:UIControlStateNormal];
+        [_button setTitle:@"Button" forState:UIControlStateNormal];
         [_button setTitleColor:[UIColor lightGrayColor] forState:UIControlStateDisabled];
         [_button addTarget:self action:@selector(clickAction:) forControlEvents:UIControlEventTouchUpInside];
         [_button setTitleColor:[UIColor colorWithRed:0.330 green:0.744 blue:0.714 alpha:1.000] forState:UIControlStateNormal];
@@ -81,7 +101,7 @@
 - (VLineButton *)anotherButton{
     if (!_anotherButton) {
         _anotherButton = [[VLineButton alloc] init];
-        _anotherButton.frame = CGRectMake(100, 200, 120, 60);
+        _anotherButton.frame = CGRectMake(100, 200, 120, 30);
         //        _anotherButton.titleLabel.font = [UIFont fontWithName:@"Avenir-Black" size:18.0];
         [_anotherButton setTitle:@"Button" forState:UIControlStateNormal];
         [_anotherButton setTitleColor:[UIColor lightGrayColor] forState:UIControlStateDisabled];
